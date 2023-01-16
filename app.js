bd30767a09aae82c819b954fbb5efd8410d6b345
Sam <sam.brown@madetech.com>
2023-01-16 17:10:43 +0000

    pull out app logic from index file into own file

---
+++ app.js
@@ -0,0 +1,30 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+var morgan = require('morgan')
+
+morgan.token('content', (req) => {
+  return JSON.stringify(req.body)
+})
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info("connected to MongoDB")
+  })
+  .catch(() => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.errorHandler)
--- index.js
+++ index.js
@@ -1,35 +1,10 @@
 const config = require('./utils/config')
 const http = require('http')
-const express = require('express')
-const app = express()
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const cors = require('cors')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
-var morgan = require('morgan')
+const app = require('./app')
 
-morgan.token('content', (req) => {
-  return JSON.stringify(req.body)
-})
+const server = http.createServer(app)
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-  .then(() => {
-    logger.info("connected to MongoDB")
-  })
-  .catch(() => {
-    logger.error('error connecting to MongoDB', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.errorHandler)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
