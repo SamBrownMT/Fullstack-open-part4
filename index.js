2f4c7f2033e7bab6e012ef584fcae9345508dab3
Sam <sam.brown@madetech.com>
2023-01-16 17:11:27 +0000

    change console log in index to use logger

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const app = require('./app')
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
