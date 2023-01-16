b4dd5fbb4a1f03b82a783db50a37dc7bb2a8def3
Sam <sam.brown@madetech.com>
2023-01-16 16:59:49 +0000

    add middleware file with error handler function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const cors = require('cors')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -20,6 +21,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
