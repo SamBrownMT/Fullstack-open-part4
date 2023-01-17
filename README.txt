94bff626481cedaaf0f90e919ffa49aff55aa1bd
Sam <sam.brown@madetech.com>
2023-01-17 13:46:27 +0000

    add favourite blog function with tests and compelete exercise 4.5

--- README.txt
+++ README.txt
@@ -2,3 +2,4 @@
 4.2 completed
 4.3 completed
 4.4 completed
+4.5 completed
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce(likesReducer, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  blogs.sort((a,b) => b.likes - a.likes)
+  return {
+    title: blogs[0].title,
+    author: blogs[0].author,
+    likes: blogs[0].likes
+  }
+}
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
