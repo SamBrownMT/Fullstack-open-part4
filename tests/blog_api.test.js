97d4f38c3273cd423556d8f53de9d3b2b7a220c1
Sam <sam.brown@madetech.com>
2023-01-19 11:11:47 +0000

    add initial pre defined database

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: "foo",
+    author: "bar",
+    url: "baz",
+    likes:420
+  },
+  {
+    title: "Hamlet",
+    author: "Shakespeare",
+    url: "Stratford",
+    likes: 1601
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api.get('/api/blogs')
